b4e14dbd91c5621e34d7a4c6f7e49895bb7069ff
trjumper <thoreijum@gmail.com>
2022-12-05 22:09:24 -0700

    Add 7 temporarily named items

--- src/main/java/com/reed/artifacts/ArtifactsMod.java
+++ src/main/java/com/reed/artifacts/ArtifactsMod.java
@@ -1,10 +1,12 @@
 package com.reed.artifacts;
 
 import com.mojang.logging.LogUtils;
+import com.reed.artifacts.init.ItemInit;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -18,20 +20,26 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("artifacts")
+@Mod(ArtifactsMod.MOD_ID)
 public class ArtifactsMod
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "artifacts";
 
     public ArtifactsMod()
     {
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        modEventBus.addListener(this::setup);
+        ItemInit.ITEMS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
