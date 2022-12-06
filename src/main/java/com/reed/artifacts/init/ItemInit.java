236a215eef2cbc660803750cd157c93da5f56b0b
trjumper <thoreijum@gmail.com>
2022-12-06 15:00:55 -0700

    Make items a-d armor items of material ANCIENT_ARTIFACT

--- src/main/java/com/reed/artifacts/init/ItemInit.java
+++ src/main/java/com/reed/artifacts/init/ItemInit.java
@@ -1,6 +1,9 @@
 package com.reed.artifacts.init;
 
 import com.reed.artifacts.ArtifactsMod;
+import com.reed.artifacts.util.AncientArtifactsMaterial;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -12,13 +15,13 @@ public class ItemInit {
     public static final DeferredRegister<Item> ITEMS=
             DeferredRegister.create(ForgeRegistries.ITEMS, ArtifactsMod.MOD_ID);
     public static final RegistryObject<Item> A = ITEMS.register("a",
-            () -> new Item(new Item.Properties().tab(ModCreativeTab.instance)));
+            () -> new ArmorItem(AncientArtifactsMaterial.ANCIENT_ARTIFACT, EquipmentSlot.HEAD, new Item.Properties().tab(ModCreativeTab.instance)));
     public static final RegistryObject<Item> B = ITEMS.register("b",
-            () -> new Item(new Item.Properties().tab(ModCreativeTab.instance)));
+            () -> new ArmorItem(AncientArtifactsMaterial.ANCIENT_ARTIFACT, EquipmentSlot.CHEST, new Item.Properties().tab(ModCreativeTab.instance)));
     public static final RegistryObject<Item> C = ITEMS.register("c",
-            () -> new Item(new Item.Properties().tab(ModCreativeTab.instance)));
+            () -> new ArmorItem(AncientArtifactsMaterial.ANCIENT_ARTIFACT, EquipmentSlot.LEGS, new Item.Properties().tab(ModCreativeTab.instance)));
     public static final RegistryObject<Item> D = ITEMS.register("d",
-            () -> new Item(new Item.Properties().tab(ModCreativeTab.instance)));
+            () -> new ArmorItem(AncientArtifactsMaterial.ANCIENT_ARTIFACT, EquipmentSlot.FEET, new Item.Properties().tab(ModCreativeTab.instance)));
     public static final RegistryObject<Item> E = ITEMS.register("e",
             () -> new Item(new Item.Properties().tab(ModCreativeTab.instance)));
     public static final RegistryObject<Item> F = ITEMS.register("f",
