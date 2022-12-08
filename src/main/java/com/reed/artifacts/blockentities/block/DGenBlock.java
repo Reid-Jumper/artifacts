a6022d08869973087b1f59c4ab10283cd88da701
trjumper <thoreijum@gmail.com>
2022-12-08 14:36:42 -0700

    Add tick functionality to tile entities

--- src/main/java/com/reed/artifacts/blockentities/block/DGenBlock.java
+++ src/main/java/com/reed/artifacts/blockentities/block/DGenBlock.java
@@ -1,10 +1,14 @@
 package com.reed.artifacts.blockentities.block;
 
 import com.reed.artifacts.init.TileEntityInit;
+import com.reed.artifacts.blockentities.entity.DGenTile;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -19,5 +23,11 @@ public class DGenBlock extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return TileEntityInit.D_GEN_BLOCK.get().create(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level world, BlockState state, BlockEntityType<T> type) {
+        return type == TileEntityInit.D_GEN_BLOCK.get() ? DGenTile::tick : null;
     }
 }
--- src/main/java/com/reed/artifacts/blockentities/block/GGenBlock.java
+++ src/main/java/com/reed/artifacts/blockentities/block/GGenBlock.java
@@ -1,10 +1,14 @@
 package com.reed.artifacts.blockentities.block;
 
+import com.reed.artifacts.blockentities.entity.FGenTile;
 import com.reed.artifacts.init.TileEntityInit;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -19,5 +23,11 @@ public class GGenBlock extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return TileEntityInit.G_GEN_BLOCK.get().create(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level world, BlockState state, BlockEntityType<T> type) {
+        return type == TileEntityInit.F_GEN_BLOCK.get() ? FGenTile::tick : null;
     }
 }
